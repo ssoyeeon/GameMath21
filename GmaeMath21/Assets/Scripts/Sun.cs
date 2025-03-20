7ab18e2e7f973e9183273dd6ec30e65568f09533
ssoyeeon <ssuu2039@gmail.com>
2025-03-20 16:14:31 +0900

    [feat] 지구 달 완료

--- GmaeMath21/Assets/Scripts/Sun.cs
+++ GmaeMath21/Assets/Scripts/Sun.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Sun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform sunn;
+    public float speed = 1f;
+    public float angle = 0f;
+    public float distance = 5f;
     void Update()
     {
-        
+        angle += Time.deltaTime * speed;
+        float radians = angle * Mathf.Deg2Rad;
+        Vector3 direction = new Vector3(Mathf.Cos(radians) * distance, 0, Mathf.Sin(radians) * distance);
+        transform.position = sunn.position + direction;
     }
 }
