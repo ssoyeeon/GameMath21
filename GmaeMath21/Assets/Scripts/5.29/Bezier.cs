86d8fc0a4adfbbd2a47fe25f7cb239d2a4270066
ssoyeeon <ssuu2039@gmail.com>
2025-05-29 15:39:52 +0900

    [fix] 엉엉

--- GmaeMath21/Assets/Scripts/5.29/Bezier.cs
+++ GmaeMath21/Assets/Scripts/5.29/Bezier.cs
@@ -21,7 +21,19 @@ public class Bezier : MonoBehaviour
     List<Vector3> points;
     float time = 0f;
     public GameObject Sphere;
+    public GameObject Sp;
 
+    [SerializeField] Transform target;
+    [SerializeField] GameObject gameObjectTarget;
+    public Vector3 offset = new Vector3(0f, 1f, -10f);
+    [SerializeField] float smoothTime = 0.3f;       //반응속도 (짧을 수록 빠름)
+    [SerializeField] float maxSpeed = 100f;     //시작 시 최고 속도
+    Vector3 velocity = Vector3.zero;
+    public LayerMask enemyLayer;
+    float speed = 2f;
+    bool isTarget;
+    bool isClick;
+    
     private void Awake()
     {
         GenerateRandomControlPoints();
@@ -31,10 +43,59 @@ public class Bezier : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime / 2f;
-        Sphere.transform.position = DeCasteljau(points,time);
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            isTarget = true;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, enemyLayer))
+            {
+                target = hit.transform;
+                gameObjectTarget = hit.collider.gameObject;
+                Quaternion lookRot = Quaternion.LookRotation(target.position - transform.position);
+
+                float t = 1f - Mathf.Exp(-speed * Time.deltaTime);
+                transform.rotation = ManualSlerp(transform.rotation, lookRot, t);
+                gameObjectTarget.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                for (int i = 0; i < 10; i++)
+                {
+                    Sp = Instantiate(Sphere, this.transform.position, Quaternion.identity);
+                }
+            }
+        }
+        Sp.transform.position = DeCasteljau(points, time);
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObjectTarget.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            isTarget = false;
+            //transform.position = new Vector3(0f,-1f,-10f);
+            Destroy(Sphere);
+            isClick = false;    
+        }
     }
-    
+
+    Quaternion ManualSlerp(Quaternion from, Quaternion to, float t)
+    {
+        float dot = Quaternion.Dot(from, to);
+        if (dot < 0f)
+        {
+            to = new Quaternion(-to.x, -to.y, -to.z, -to.w);
+            dot = -dot;
+        }
+        float theta = Mathf.Acos(dot);
+        float sinTheta = Mathf.Sin(theta);
+
+        float ratioA = Mathf.Sin((1f - t) * theta) / sinTheta;
+        float ratioB = Mathf.Sin(t * theta) / sinTheta;
+
+        Quaternion result = new Quaternion(
+            ratioA * from.x + ratioB * to.x,
+            ratioA * from.y + ratioB * to.y,
+            ratioA * from.z + ratioB * to.z,
+            ratioA * from.w + ratioB * to.w
+        );
+        return result.normalized;
+    }
     void GenerateRandomControlPoints()
     {
         Vector2 rand1 = Random.insideUnitCircle * p1Radius;
