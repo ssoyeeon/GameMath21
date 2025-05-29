b17d50c0864c9a58d2b3614b64d687679fd85fde
ssoyeeon <ssuu2039@gmail.com>
2025-05-29 16:32:53 +0900

    [fix] 완성!!

--- GmaeMath21/Assets/Scripts/5.29/Bezier.cs
+++ GmaeMath21/Assets/Scripts/5.29/Bezier.cs
@@ -20,8 +20,7 @@ public class Bezier : MonoBehaviour
 
     List<Vector3> points;
     float time = 0f;
-    public GameObject Sphere;
-    public GameObject Sp;
+    public BezierSpehee Sphere;
 
     [SerializeField] Transform target;
     [SerializeField] GameObject gameObjectTarget;
@@ -36,8 +35,7 @@ public class Bezier : MonoBehaviour
     
     private void Awake()
     {
-        GenerateRandomControlPoints();
-        points = new List<Vector3> { p0.position, p1, p2, p3.position };
+
     }
     // Update is called once per frame
     void Update()
@@ -59,11 +57,13 @@ public class Bezier : MonoBehaviour
                 gameObjectTarget.gameObject.GetComponent<Renderer>().material.color = Color.red;
                 for (int i = 0; i < 10; i++)
                 {
-                    Sp = Instantiate(Sphere, this.transform.position, Quaternion.identity);
+                    BezierSpehee Sp = Instantiate(Sphere, this.transform.position, Quaternion.identity);
+                    Sp.p0 = p0;
+                    Sp.p3 = p3;
+                    Sp.asd();
                 }
             }
         }
-        Sp.transform.position = DeCasteljau(points, time);
         if (Input.GetMouseButtonDown(1))
         {
             gameObjectTarget.gameObject.GetComponent<Renderer>().material.color = Color.white;
@@ -95,27 +95,5 @@ public class Bezier : MonoBehaviour
             ratioA * from.w + ratioB * to.w
         );
         return result.normalized;
-    }
-    void GenerateRandomControlPoints()
-    {
-        Vector2 rand1 = Random.insideUnitCircle * p1Radius;
-        p1 = p0.position + new Vector3(rand1.x, 0f, rand1.y);
-        p1.y += p1Height;
-
-        Vector2 rand2 = Random.insideUnitCircle * p2Radius;
-        p2 = p3.position + new Vector3(rand2.x, 0f, rand2.y);
-        p2.y += p2Height;
-    }
-    Vector3 DeCasteljau(List<Vector3> p, float t)
-    {
-        while(p.Count > 1)
-        {
-            int last = p.Count - 1;
-            var next = new List<Vector3>(last);
-            for (int i = 0; i < last; i++)
-                next.Add(Vector3.Lerp(p[i], p[i + 1], t));
-            p = next;
-        }
-        return p[0];
     }
 }
---
+++ GmaeMath21/Assets/Scripts/5.29/BezierSpehee.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierSpehee : MonoBehaviour
+{
+    public Transform p0;
+
+    public Transform p3;
+
+    [Header("Random Ranges")]
+    public float p1Radius = 2f;
+    public float p1Height = 2f;
+    public float p2Radius = 2f;
+    public float p2Height = 2f;
+
+    public Vector3 p1;
+    public Vector3 p2;
+
+    List<Vector3> points;
+    float time = 0f;
+    // Start is called before the first frame update
+    public void asd()
+    {
+        GenerateRandomControlPoints();
+        points = new List<Vector3> { p0.position, p1, p2, p3.position };
+    }
+    private void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.position = DeCasteljau(points, time);
+
+    }
+    void GenerateRandomControlPoints()
+    {
+        Vector2 rand1 = Random.insideUnitCircle * p1Radius;
+        p1 = p0.position + new Vector3(rand1.x, 0f, rand1.y);
+        p1.y += p1Height;
+
+        Vector2 rand2 = Random.insideUnitCircle * p2Radius;
+        p2 = p3.position + new Vector3(rand2.x, 0f, rand2.y);
+        p2.y += p2Height;
+    }
+    Vector3 DeCasteljau(List<Vector3> p, float t)
+    {
+        while (p.Count > 1)
+        {
+            int last = p.Count - 1;
+            var next = new List<Vector3>();
+            for (int i = 0; i < last; i++)
+                next.Add(Vector3.Lerp(p[i], p[i + 1], t));
+            p = next;
+        }
+        return p[0];
+    }
+}
