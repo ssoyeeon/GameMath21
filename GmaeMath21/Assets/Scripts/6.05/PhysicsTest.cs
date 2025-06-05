b6d2ffc5d3273e31aeea9b7fc264d6d4ccffed1f
ssoyeeon <ssuu2039@gmail.com>
2025-06-05 15:54:02 +0900

    [feat] 모르겠어요 ㅠ

--- GmaeMath21/Assets/Scripts/6.05/PhysicsTest.cs
+++ GmaeMath21/Assets/Scripts/6.05/PhysicsTest.cs
@@ -7,30 +7,52 @@ using UnityEngine.UI;
 
 public class PhysicsTest : MonoBehaviour
 {
+    bool isEnd;     //false 일 경우 플레이어 1
+    Rigidbody rbb;
+    public Rigidbody p1;
+    public Rigidbody p2;
+
     // Start is called before the first frame update
     void Start()
-    { 
-
+    {
+        isEnd = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (rbb != null)
+        {
+            if (rbb.velocity.magnitude <= 0.1 && isEnd == false)
+            {
+                isEnd = true;
+                Debug.Log("Stop");
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && rbb.velocity.magnitude <= 0.1)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if(Physics.Raycast(ray, out RaycastHit hit))
             {
                 Rigidbody rb = hit.collider.attachedRigidbody;
-
-                if ( rb != null)
+                rbb = rb;
+                if ( rb != null && isEnd == false)
                 {
                     Vector3 move =  hit.collider.transform.position - hit.point;
-                    rb.AddForce(move * 35f, ForceMode.Impulse);
+                    p1.AddForce(move * 40f, ForceMode.Impulse);
+                }
+                else if (rb != null && isEnd == true)
+                {
+                    Vector3 move = hit.collider.transform.position - hit.point;
+                    p2.AddForce(move * 40f, ForceMode.Impulse);
+                    isEnd = false;
                 }
                 
             }
+
         }
     }
 }
